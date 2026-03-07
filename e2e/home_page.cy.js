772983969525aa7995b7e8e4560f5580e217677e
Yan Costa <yan.felipe.cds@gmail.com>
2026-03-06 21:58:15 -0300

    Push a new scenario

--- e2e/home_page.cy.js
+++ e2e/home_page.cy.js
@@ -7,10 +7,17 @@ describe('Home page scenario', () => {
         cy.get(xpath.homePage.removeCartButton).should('is.visible')
     })
 
-    it.only('TS009 - Removing the product to the cart', () => {
+    it('TS009 - Removing the product to the cart', () => {
         cy.successfulLogin()
         cy.get(xpath.homePage.backpackAddCart).click()
         cy.get(xpath.homePage.removeCartButton).click()
         cy.get(xpath.homePage.backpackAddCart).should('is.visible')
+    })
+
+    it.only('TS010 - Using a filter name Name (Z to A)', () => {
+        cy.successfulLogin()
+        cy.get(xpath.homePage.filter).select('Name (Z to A)')
+        cy.get(xpath.homePage.First_product).should('is.visible').contains('Test.allTheThings() T-Shirt (Red)')
+        
     })
 })
--- support/xpath.js
+++ support/xpath.js
@@ -11,6 +11,8 @@ export const xpath = {
     buttonMenu: "//button[@id='react-burger-menu-btn']",
     logout: "//a[@id='logout_sidebar_link']",
     backpackAddCart: '[data-test="add-to-cart-sauce-labs-backpack"]',
-    removeCartButton: '[data-test="remove-sauce-labs-backpack"]'
+    removeCartButton: '[data-test="remove-sauce-labs-backpack"]',
+    filter : '[data-test="product-sort-container"]',
+    First_product: ':nth-child(1) > [data-test="inventory-item-description"]'
     }
 }
